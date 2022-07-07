b5db28ca6c79e09b006631638570bc0b7e76e0ff
Davi Belo <davibelocontato@gmail.com>
2022-07-07 19:04:38 -0300

    first commit

---
+++ index.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+
+int main(){
+       
+    int num; 
+
+    for(int i = 0;i==0;){
+        cin>>num;
+        if(num==0){
+            break;
+        }
+        else {
+            cout<<num*(-1)<<endl;
+        }
+    }
+    
+    return 0;
+}
+
